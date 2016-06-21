7cb053110c64ad2a8de08764ed2240ad30bb48f3
Duncan Hall <himself@duncanhall.net>
2016-06-21 16:42:19 +0100

    Proxy request to forecast API

--- app/weatherClient.js
+++ app/weatherClient.js
@@ -1,7 +1,26 @@
 'use strict';
 
+var bluebird = require('bluebird');
+var request = bluebird.promisify(require('request'));
+
+var API_KEY = '856add1ae87b1ec3d33a90ab024c61c0';
+
+
 function getForecast(req, res) {
-    res.send('RETURN FORECAST');
+
+    var location = req.params.locationId;
+
+    request({
+        url: 'http://api.openweathermap.org/data/2.5/forecast?mode=json&appid=' + API_KEY + '&id=' + location,
+        json: true,
+        method: 'GET'
+    }).then(function(result) {
+        if (result.body.cod !== "200") {
+            res.send(500);
+        } else {
+            res.json(result.body)
+        }
+    });
 }
 
 module.exports = {
