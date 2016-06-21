44f1e49554148b4539646f2d7548dad41cadf656
Duncan Hall <himself@duncanhall.net>
2016-06-21 19:31:56 +0100

    Setup heroku port and procfile

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var routes = require(path.resolve('app/routes'));
+var PORT = (process.env.PORT || 3000);
 
 app.use(routes);
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
     console.log('Server started on 3000');
 });
